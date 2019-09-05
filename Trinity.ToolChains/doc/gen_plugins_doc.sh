51923e5a5297c75536ecd7cd946835a013731bb0
Benjamin Piette <benjamin@findizer.fr>
2019-09-05 12:00:28 +0800

    Script to build trinity plugins API reference using jsdoc

---
+++ Trinity.ToolChains/doc/gen_plugins_doc.sh
@@ -0,0 +1,4 @@
+## Preliminary dependency: npm i -g jsdoc
+## Will look for the Elastos.Trinity repo at the same level as this repo to generate Trinity plugins documentation
+
+jsdoc -r  ../../Elastos.Trinity/Plugins/

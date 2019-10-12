baf3f254523f42f9e66cf3fd88a2e085862c8619
Benjamin Piette <benjamin@findizer.fr>
2019-10-12 15:18:24 +0800

    Removed useless resources from template dapp + updated ionic/angular version in template + new NPM version for CLI

--- Trinity.ToolChains/cli/src/assets/dapptemplate/src/app/app.component.ts
+++ Trinity.ToolChains/cli/src/assets/dapptemplate/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar/ngw';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 
 import { TabsPage } from '../pages/tabs/tabs';
 
--- Trinity.ToolChains/cli/src/assets/dapptemplate/src/app/app.module.ts
+++ Trinity.ToolChains/cli/src/assets/dapptemplate/src/app/app.module.ts
@@ -8,8 +8,8 @@ import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 
 @NgModule({
   declarations: [

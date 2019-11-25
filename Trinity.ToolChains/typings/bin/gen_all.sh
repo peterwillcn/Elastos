e96758b1cf9eff950bd847ceaae4ec93836c34aa
Benjamin Piette <benjamin@findizer.fr>
2019-11-25 09:22:47 +0800

    Added Hive Typescript

--- Trinity.ToolChains/typings/bin/gen_all.sh
+++ Trinity.ToolChains/typings/bin/gen_all.sh
@@ -9,6 +9,10 @@ echo "Copying Carrier types..."
 cat $pluginsroot/Carrier/www/types.d.ts > $outdir/carrier.d.ts
 echo "Copying Wallet types..."
 cat $pluginsroot/Wallet/www/types.d.ts > $outdir/wallet.d.ts
+echo "Copying DID types..."
+cat $pluginsroot/DID/www/types.d.ts > $outdir/did.d.ts
+echo "Copying Hive types..."
+cat $pluginsroot/Hive/www/types.d.ts > $outdir/hive.d.ts
 
 echo ""
 echo "DONE - Don't forget to publish this new version on NPM!"

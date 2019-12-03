fb84ac25f2fafde785986b2f582be4f0e54c379b
Benjamin Piette <benjamin.piette@elastos-fusion.org>
2019-12-03 16:39:29 +0800

    Adjusted typings build script to run inside the Elastos.Trinity repo

--- Trinity.ToolChains/doc/gen_plugins_doc.sh
+++ Trinity.ToolChains/doc/gen_plugins_doc.sh
@@ -4,6 +4,6 @@
 # DocStrap template documentation: https://github.com/docstrap/docstrap
 
 # First need to build trinity in order to apply plugins documentation patches
-../../Elastos.Trinity/ToolChains/bin/build all
+../../ToolChains/bin/build all
 # Generate the documentation
 typedoc ../typings/dist --out ./out --mode file --tsconfig ./tsconfig.json --theme ./trinity-plugins-theme --readme ./README.md --name "Trinity Plugins API Reference" --excludeExternals --includeDeclarations

59c75f38e9ac141bb5dee224ee53addc99615071
Benjamin Piette <benjamin.piette@elastos-fusion.org>
2019-12-05 07:28:03 +0800

    Fixed DID lib import issues

--- Trinity.ToolChains/cli/scripts/getnativedid.sh
+++ Trinity.ToolChains/cli/scripts/getnativedid.sh
@@ -1,4 +1,4 @@
 mkdir -p bin
 mkdir -p bin/lib
 wget https://github.com/elastos/Elastos.Trinity.ToolChains/releases/download/release-eladid-v1.0.0/libeladid.1.dylib -O bin/lib/libeladid.1.dylib
-wget https://github.com/elastos/Elastos.Trinity.ToolChains/releases/download/release-eladid-v1.0.0/libeladid.1.so -O bin/lib/libeladid.1.so
+wget https://github.com/elastos/Elastos.Trinity.ToolChains/releases/download/release-eladid-v1.0.0/libeladid.so.1 -O bin/lib/libeladid.so.1

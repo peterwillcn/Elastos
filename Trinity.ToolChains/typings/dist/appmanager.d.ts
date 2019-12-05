b0c88ee5fb8cf6d98a893b9559fec9cc7e58613c
Benjamin Piette <benjamin.piette@elastos-fusion.org>
2019-12-05 13:20:45 +0800

    Credential changes in DID + documentation

--- Trinity.ToolChains/typings/dist/appmanager.d.ts
+++ Trinity.ToolChains/typings/dist/appmanager.d.ts
@@ -1,12 +1,6 @@
 declare namespace AppManagerPlugin {
     /**
      * The icons info.
-     *
-     * @typedef Icon
-     * @type {Object}
-     * @property {string}           src         The icon src.
-     * @property {string}           sizes       The icon sizes.
-     * @property {string}           type        The icon type.
      */
     type Icon = {
         /** The icon src. */
@@ -55,11 +49,6 @@ declare namespace AppManagerPlugin {
 
     /**
      * The plugin authority status.
-     *
-     * @typedef PluginAuthority
-     * @type {Object}
-     * @property {string}           plugin      The plugin name.
-     * @property {AuthorityStatus}  authority   The authority status.
      */
     type PluginAuthority = {
         /** The plugin name. */
@@ -70,11 +59,6 @@ declare namespace AppManagerPlugin {
 
     /**
      * The access url authority status.
-     *
-     * @typedef UrlAuthority
-     * @type {Object}
-     * @property {string}           url         The url access.
-     * @property {AuthorityStatus}  authority   The authority status.
      */
     type UrlAuthority = {
         /** The url access. */
@@ -85,31 +69,6 @@ declare namespace AppManagerPlugin {
 
     /**
      * The App information.
-     *
-     * @typedef AppInfo
-     * @type {Object}
-     * @param          id              The app id.
-     * @param          version         The app version.
-     * @param          name            The app name.
-     * @param          shortName       The app shortName.
-     * @param          description     The app description.
-     * @param          startUrl        The app startUrl.
-     * @param          icons           The app icons.
-     * @param          authorName      The app authorName.
-     * @param          authorEmail     The app authorEmail.
-     * @param          defaultLocale   The app defaultLocale.
-     * @param          category        The app category.
-     * @param          keyWords        The app keyWords.
-     * @param          plugins         The app PluginAuthority list.
-     * @param          urls            The app UrlAuthoritylist.
-     * @param          backgroundColor The app backgroundColor.
-     * @param          themeDisplay    The app theme display.
-     * @param          themeColor      The app theme color.
-     * @param          themeFontName   The app theme font name.
-     * @param          themeFontColor  The app theme font color.
-     * @param          installTime     The app intall time.
-     * @param          builtIn         The app builtIn.
-     * @param          dataPath        The app data path.
      */
     type AppInfo = {
         /** The app id. */
@@ -162,10 +121,6 @@ declare namespace AppManagerPlugin {
 
     /**
      * Object received when receiving a message.
-     *
-     * @param {string}  msg		    The message receive
-     * @param {number}  type	    The message type
-     * @param {string}  from		The message from
      */
     type ReceivedMessage = {
         /** The message receive */
@@ -178,10 +133,6 @@ declare namespace AppManagerPlugin {
 
     /**
      * Object received when receiving an intent.
-     *
-     * @param {string}  action		 The intent action
-     * @param {Object}  params	    The intent params
-     * @param {string}  from		The intent from
      */
     type ReceivedIntent = {
         /** The intent action */
@@ -194,222 +145,221 @@ declare namespace AppManagerPlugin {
 
     /**
      * The class representing dapp manager for launcher.
-     * @class
      */
     interface AppManager {
         /**
          * Get locale.
          *
-         * @param {Function} onSuccess  The function to call.the param include 'defaultLang' and 'systemLang'.
+         * @param onSuccess  The function to call.the param include 'defaultLang' and 'systemLang'.
          */
         getLocale(onSuccess: (defaultLang: string, systemLang: string)=>void);
 
         /**
          * Set current locale.
          *
-         * @param {string}   code       The current locale code.
-         * @param {Function} onSuccess  The function to call when success.the param is a AppInfo.
-         * @param {Function} [onError]  The function to call when error, the param is a String. Or set to null.
+         * @param code       The current locale code.
+         * @param onSuccess  The function to call when success.the param is a AppInfo.
+         * @param onError    The function to call when error, the param is a String. Or set to null.
          */
         setCurrentLocate(code: string, onSuccess:(appInfo: AppInfo)=>void, onError?:(err:string)=>void);
 
         /**
          * Install a dapp by path.
          *
-         * @param {string}   url        The dapp install url.
-         * @param {boolean}  update    The dapp install update.
-         * @param {Function} onSuccess  The function to call when success.the param is a AppInfo.
-         * @param {Function} [onError]  The function to call when error, the param is a String. Or set to null.
+         * @param url        The dapp install url.
+         * @param update     The dapp install update.
+         * @param onSuccess  The function to call when success.the param is a AppInfo.
+         * @param onError    The function to call when error, the param is a String. Or set to null.
          */
         install(url: string, update: Boolean, onSuccess:(appInfo: AppInfo)=>void, onError?:(err: string)=>void);
 
         /**
          * Uninstall a dapp by id.
          *
-         * @param {string}   id         The dapp id.
-         * @param {Function} onSuccess  The function to call when success.the param is the id.
-         * @param {Function} [onError]  The function to call when error, the param is a String. Or set to null.
+         * @param id         The dapp id.
+         * @param onSuccess  The function to call when success.the param is the id.
+         * @param onError    The function to call when error, the param is a String. Or set to null.
          */
         unInstall(id: string, onSuccess:(id: string)=>void, onError?:(err: string)=>void);
 
         /**
          * Get dapp info.
          *
-         * @param {Function} onSuccess  The function to call when success, the param is a AppInfo.
-         * @param {Function} [onError]  The function to call when error, the param is a String. Or set to null.
+         * @param onSuccess  The function to call when success, the param is a AppInfo.
+         * @param onError    The function to call when error, the param is a String. Or set to null.
          */
         getInfo(onSuccess:(appInfo: AppInfo)=>void, onError?:(err: string)=>void);
 
         /**
          * Get a dapp info.
          *
-         * @param {string}   id       The dapp id.
-         * @param {Function} onSuccess  The function to call when success, the param is a AppInfo.
-         * @param {Function} [onError]  The function to call when error, the param is a String. Or set to null.
+         * @param id         The dapp id.
+         * @param onSuccess  The function to call when success, the param is a AppInfo.
+         * @param onError    The function to call when error, the param is a String. Or set to null.
          */
         getAppInfo(id: string, onSuccess:(appInfo: AppInfo)=>void, onError?:(err: string)=>void);
 
         /**
          * Get a dapp info.
          *
-         * @param {Function} onSuccess  The function to call when success, the param is include 'infos' and 'list'.
+         * @param onSuccess  The function to call when success, the param is include 'infos' and 'list'.
          */
         getAppInfos(onSuccess:(appsInfo: AppInfo[])=>void);
 
         /**
          * Start a dapp by id. If the dapp running, it will be swith to curent.
          *
-         * @param {string}   id         The dapp id.
-         * @param {Function} onSuccess  The function to call when success.
-         * @param {Function} [onError]  The function to call when error, the param is a String. Or set to null.
+         * @param id         The dapp id.
+         * @param onSuccess  The function to call when success.
+         * @param onError    The function to call when error, the param is a String. Or set to null.
          */
         start(id: string, onSuccess:()=>void, onError?:(err: string)=>void);
 
         /**
          * Start the launcher.If the launcher running, it will be swith to curent.
          *
-         * @param {Function} onSuccess  The function to call when success.
-         * @param {Function} [onError]  The function to call when error, the param is a String. Or set to null.
+         * @param onSuccess  The function to call when success.
+         * @param onError    The function to call when error, the param is a String. Or set to null.
          */
         launcher(onSuccess?:()=>void, onError?:(err: string)=>void);
 
         /**
          * Close dapp.
          *
-         * @param {Function} onSuccess  The function to call when success.
-         * @param {Function} [onError]  The function to call when error, the param is a String. Or set to null.
+         * @param onSuccess  The function to call when success.
+         * @param onError    The function to call when error, the param is a String. Or set to null.
          */
         close(onSuccess?:()=>void, onError?:(err: string)=>void);
 
         /**
          * Close a dapp by id.
          *
-         * @param {string}   id         The dapp id.
-         * @param {Function} onSuccess  The function to call when success.
-         * @param {Function} [onError]  The function to call when error, the param is a String. Or set to null.
+         * @param id         The dapp id.
+         * @param onSuccess  The function to call when success.
+         * @param onError    The function to call when error, the param is a String. Or set to null.
          */
         closeApp(id: string, onSuccess?:()=>void, onError?:(err: string)=>void);
 
         /**
          * Send a message by id.
          *
-         * @param {string}          id      The dapp id.
-         * @param {MessageType}     type    The message type.
-         * @param {string}   msg        The message content.
-         * @param {Function} onSuccess  The function to call when success.
-         * @param {Function} [onError]  The function to call when error, the param is a String. Or set to null.
+         * @param id         The dapp id.
+         * @param type       The message type.
+         * @param msg        The message content.
+         * @param onSuccess  The function to call when success.
+         * @param onError    The function to call when error, the param is a String. Or set to null.
          */
         sendMessage(id: string, type: MessageType, msg: string, onSuccess:()=>void, onError?:(err: string)=>void);
 
         /**
          * Set listener for message callback.
          *
-         * @param {Function} callback   The function receive the message.
+         * @param callback   The function receive the message.
          */
         setListener(callback: (msg: ReceivedMessage)=>void);
 
         /**
          * Get running list.
          *
-         * @param {Function} onSuccess  The function to call when success,the param is a dapp id list.
+         * @param onSuccess  The function to call when success,the param is a dapp id list.
          */
         getRunningList(onSuccess:(ids: string[])=>void);
 
         /**
          * Get dapp list.
          *
-         * @param {Function} onSuccess  The function to call when success,the param is a dapp id list.
+         * @param onSuccess  The function to call when success,the param is a dapp id list.
          */
         getAppList(onSuccess:(ids: string[])=>void);
 
         /**
          * Get last run list.
          *
-         * @param {Function} onSuccess  The function to call when success,the param is a dapp id list.
+         * @param onSuccess  The function to call when success,the param is a dapp id list.
          */
         getLastList(onSuccess:(ids: string[])=>void);
 
         /**
          * Set a plugin authority. Only the launcher can set.
          *
-         * @param {string}   id       The dapp id.
-         * @param {string}   plugin     The plugin id to set authorty.
-         * @param {AuthorityStatus}   authority  The authority to set.
-         * @param {Function} onSuccess  The function to call when success.
-         * @param {Function} [onError]  The function to call when error, the param is a String. Or set to null.
+         * @param id         The dapp id.
+         * @param plugin     The plugin id to set authorty.
+         * @param authority  The authority to set.
+         * @param onSuccess  The function to call when success.
+         * @param onError    The function to call when error, the param is a String. Or set to null.
          */
         setPluginAuthority(id: string, plugin: string, authority: PluginAuthority, onSuccess: ()=>void, onError: (err:any)=>void);
 
         /**
          * Set a url authority. Only the launcher can set.
          *
-         * @param {string}   id       The dapp id.
-         * @param {string}   url      The url to set authority.
-         * @param {AuthorityStatus}   authority  The authority to set.
-         * @param {Function} onSuccess  The function to call when success.
-         * @param {Function} [onError]  The function to call when error, the param is a String. Or set to null.
+         * @param id         The dapp id.
+         * @param url        The url to set authority.
+         * @param authority  The authority to set.
+         * @param onSuccess  The function to call when success.
+         * @param onError    The function to call when error, the param is a String. Or set to null.
          */
         setUrlAuthority(id: string, url: string, authority: UrlAuthority, onSuccess: ()=>void, onError: (err:any)=>void);
 
         /**
          * Display a alert dialog prompt.
          *
-         * @param {string}   title       The dialog title.
-         * @param {string}   message     The dialog message.
+         * @param title       The dialog title.
+         * @param message     The dialog message.
          */
         alertPrompt(title: string, message: string);
 
         /**
          * Display a info dialog prompt.
          *
-         * @param {string}   title       The dialog title.
-         * @param {string}   message     The dialog message.
+         * @param title       The dialog title.
+         * @param message     The dialog message.
          */
         infoPrompt(title: string, message: string);
 
         /**
          * Display a ask dialog prompt.
          *
-         * @param {string}   title       The dialog title.
-         * @param {string}   message     The dialog message.
-         * @param {Function} onOK        The function to call when click ok.
+         * @param title       The dialog title.
+         * @param message     The dialog message.
+         * @param onOK        The function to call when click ok.
          */
         askPrompt(title: string, message: string, onOK:()=>void);
 
         /**
          * Send a intent by action.
          *
-         * @param {string}   action     The intent action.
-         * @param {Object}   params     The intent params.
-         * @param {Function} onSuccess  The function to call when success.
-         * @param {Function} [onError]  The function to call when error, the param is a String. Or set to null.
+         * @param action     The intent action.
+         * @param params     The intent params.
+         * @param onSuccess  The function to call when success.
+         * @param onError    The function to call when error, the param is a String. Or set to null.
          */
         sendIntent(action: string, params: any, onSuccess?: (ret: any)=>void, onError?: (err:any)=>void);
 
         /**
          * Send a intent by url.
          *
-         * @param {string}   url     The intent url.
-         * @param {Function} onSuccess  The function to call when success.
-         * @param {Function} [onError]  The function to call when error, the param is a String. Or set to null.
+         * @param url        The intent url.
+         * @param onSuccess  The function to call when success.
+         * @param onError    The function to call when error, the param is a String. Or set to null.
          */
         sendUrlIntent(url: string, onSuccess: ()=>void, onError: (err:any)=>void);
 
         /**
          * Set intent listener for message callback.
          *
-         * @param {ReceivedIntent} callback   The function receive the intent.
+         * @param callback   The function receive the intent.
          */
         setIntentListener(callback: (msg: ReceivedIntent)=>void);
 
         /**
          * Send a intent response by id.
          *
-         * @param {string}   action     The intent action.
-         * @param {Object}   result     The intent response result.
-         * @param {long}     intentId   The intent id.
-         * @param {Function} onSuccess  The function to call when success.
-         * @param {Function} [onError]  The function to call when error, the param is a String. Or set to null.
+         * @param action     The intent action.
+         * @param result     The intent response result.
+         * @param intentId   The intent id.
+         * @param onSuccess  The function to call when success.
+         * @param onError    The function to call when error, the param is a String. Or set to null.
          */
         sendIntentResponse(action: string, result: any, intentId: Number, onSuccess: (response: any)=>void, onError?: (err:any)=>void);
     }

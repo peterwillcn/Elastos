d2f633104c5951551eeaff116e6f2cedd3f8dae9
Benjamin Piette <benjamin.piette@elastos-fusion.org>
2020-01-02 17:24:43 +0800

    Build only runtime to generate trinity doc, not all

--- Trinity.ToolChains/doc/gen_plugins_doc.sh
+++ Trinity.ToolChains/doc/gen_plugins_doc.sh
@@ -4,7 +4,7 @@
 # DocStrap template documentation: https://github.com/docstrap/docstrap
 
 # First need to build trinity in order to apply plugins documentation patches
-../../ToolChains/bin/build all
+../../ToolChains/bin/build runtime
 # Generate the documentation
 
 typedoc ../typings/dist --out ./out --mode file --tsconfig ./tsconfig.json --theme ./trinity-plugins-theme --readme ./README.md --name "Trinity Plugins API Reference" --excludeExternals --includeDeclarations

c15c2623c9a44bf4c4924c805987e0b5df095bba
Benjamin Piette <benjamin.piette@elastos-fusion.org>
2020-02-06 16:16:17 +0800

    Starting app invisible by default + make it visible when ready (angular template)

--- Trinity.ToolChains/cli/src/assets/dapptemplate/angular/basic/src/app/pages/home/home.ts
+++ Trinity.ToolChains/cli/src/assets/dapptemplate/angular/basic/src/app/pages/home/home.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { NavController } from '@ionic/angular';
 
+declare let appManager: AppManagerPlugin.AppManager;
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
   constructor(public navCtrl: NavController) {
+  }
+
+  ionViewDidEnter() {
+    // When the main screen is ready to be displayed, ask the app manager to make the app visible,
+    // in case it was started hidden while loading.
+    appManager.setVisible("show");
   }
 }

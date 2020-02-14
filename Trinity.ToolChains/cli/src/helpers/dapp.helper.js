648eea609dc15563e65f5472f5a7963709db23ce
Benjamin Piette <benjamin.piette@elastos-fusion.org>
2020-02-14 13:02:15 +0800

    Updated missing dapp.helper.js file for unicode filename issue

--- Trinity.ToolChains/cli/src/helpers/dapp.helper.js
+++ Trinity.ToolChains/cli/src/helpers/dapp.helper.js
@@ -1,7 +1,7 @@
-const path = require("path")
-const fs = require("fs")
-const os = require("os")
-require("colors")
+const path = require("path");
+const fs = require("fs");
+const os = require("os");
+require("colors");
 
 const ManifestHelper = require("./manifest.helper");
 const IonicHelper = require("./ionic.helper");
@@ -12,23 +12,23 @@ module.exports = class DAppHelper {
      * Make sure current folder is a DApp, to not try to package some invalid content.
      */
     checkFolderIsDApp() {
-        let manifestHelper = new ManifestHelper()
-        let ionicHelper = new IonicHelper()
+        let manifestHelper = new ManifestHelper();
+        let ionicHelper = new IonicHelper();
 
         // Older dapps csanity check. Warn user to not use a manifest.json in the root folder because
         // we will use the one from the src/assets folder
-        var rootManifestPath = path.join(process.cwd(), "manifest.json")
+        var rootManifestPath = path.join(process.cwd(), "manifest.json");
         if (fs.existsSync(rootManifestPath)) {
-            console.log("WARNING".yellow + ": You should not have manifest.json in your root folder. It has to be in src/assts instead.")
+            console.log("WARNING".yellow + ": You should not have manifest.json in your root folder. It has to be in src/assts instead.");
         }
 
         // Check manifest.json  manifestHelper.getManifestPath(info.framework)
-        var ionicConfig = ionicHelper.getConfig()
+        var ionicConfig = ionicHelper.getConfig();
         if (!ionicConfig) {
             return false;
         }
 
-        var manifestPath = manifestHelper.getManifestPath(ionicConfig.assets_path)
+        var manifestPath = manifestHelper.getManifestPath(ionicConfig.assets_path);
         if (!fs.existsSync(manifestPath)) {
             return false;
         }
@@ -39,11 +39,11 @@ module.exports = class DAppHelper {
     }
 
     noManifestErrorMessage() {
-        return "Current folder is not a trinity dapp. Make sure there is a manifest.json in src/assets, or create one using the *manifest* CLI command."
+        return "Current folder is not a trinity dapp. Make sure there is a manifest.json in src/assets, or create one using the *manifest* CLI command.";
     }
 
     getTempEPKPath() {
-        return os.tmpdir()+"/temp.epk"
+        return os.tmpdir() + "/temp.epk";
     }
 
     /**
@@ -51,40 +51,45 @@ module.exports = class DAppHelper {
      */
     packEPK(manifestPath) {
         return new Promise((resolve, reject) => {
-            console.log("Packaging current folder into a Elastos package (EPK) file...")
+            console.log("Packaging current folder into a Elastos package (EPK) file...");
 
-            let dappHelper = new DAppHelper()
-            let manifestHelper = new ManifestHelper()
-            let ionicHelper = new IonicHelper()
+            let dappHelper = new DAppHelper();
+            let manifestHelper = new ManifestHelper();
+            let ionicHelper = new IonicHelper();
 
             if (!dappHelper.checkFolderIsDApp()) {
                 reject(manifestHelper.noManifestErrorMessage());
                 return;
             }
 
-            var rootScriptDirectory = path.dirname(require.main.filename)
-            var outputEPKPath = this.getTempEPKPath()
+            var rootScriptDirectory = path.dirname(require.main.filename);
+            var outputEPKPath = this.getTempEPKPath();
 
-            console.log("Output EPK will generated at: "+outputEPKPath)
+            console.log("Output EPK will generated at: " + outputEPKPath);
 
             const spawn = require("child_process").spawn;
-            const pythonProcess = spawn('python',[rootScriptDirectory+"/toolchain/pack_epk", outputEPKPath, "-r",".","--root-dir",ionicHelper.getConfig().dist_path,"-m",manifestPath]);
+            const pythonProcess = spawn('python3', [rootScriptDirectory + "/toolchain/pack_epk", outputEPKPath, "-r", ".", "--root-dir", ionicHelper.getConfig().dist_path, "-m", manifestPath]);
 
-            pythonProcess.stdout.on('data', function (data) { console.log(''+data)});
-            pythonProcess.stderr.on('data', function (data) { console.log(''+data)});
-            pythonProcess.on('error', function(err) { reject(err)})
+            pythonProcess.stdout.on('data', function (data) {
+                console.log('' + data);
+            });
+            pythonProcess.stderr.on('data', function (data) {
+                console.log('' + data);
+            });
+            pythonProcess.on('error', function (err) {
+                reject(err);
+            });
 
             pythonProcess.on('exit', function (code) {
                 if (code == 0) {
                     // Packed the EPK successfully
-                    console.log("EPK packaged successfully")
-                    resolve(outputEPKPath)
-                }
-                else {
-                    reject('Child process exited with code ' + code)
+                    console.log("EPK packaged successfully");
+                    resolve(outputEPKPath);
+                } else {
+                    reject('Child process exited with code ' + code);
                 }
             });
-        })
+        });
     }
 
     /**
@@ -92,35 +97,40 @@ module.exports = class DAppHelper {
      */
     signEPK(EPKPath, didURL, didSignaturePassword, didStorePath = DIDHelper.DEFAULT_DID_STORE_FOLDER_NAME) {
         return new Promise((resolve, reject) => {
-            console.log("Signing the generated EPK with your identity...")
+            console.log("Signing the generated EPK with your identity...");
 
             // TMP DEV: did:elastos:ikFN4BuFYYvR9ERNTiWH1jAdBztE3J691m#primary
             // TMP DEV: did:elastos:ikFN4BuFYYvR9ERNTiWH1jAdBztE3J691m#primary
-            
-            var rootScriptDirectory = path.dirname(require.main.filename)
+
+            var rootScriptDirectory = path.dirname(require.main.filename);
             //var idKeystorePath = nupath.join(process.cwd(), "appdid");
-            var signedEPKPath = EPKPath+"_signed";
- 
+            var signedEPKPath = EPKPath + "_signed";
+
             //console.log("path",idKeystorePath)
 
             const spawn = require("child_process").spawn;
-            const pythonProcess = spawn('python',[rootScriptDirectory+"/toolchain/did_sign", "-r",didStorePath,"-u",didURL,"-s",didSignaturePassword,"-o",signedEPKPath,EPKPath]);
+            const pythonProcess = spawn('python3', [rootScriptDirectory + "/toolchain/did_sign", "-r", didStorePath, "-u", didURL, "-s", didSignaturePassword, "-o", signedEPKPath, EPKPath]);
 
-            pythonProcess.stdout.on('data', function (data) { console.log(''+data)});
-            pythonProcess.stderr.on('data', function (data) { console.log(''+data)});
-            pythonProcess.on('error', function(err) { console.log(err); reject(err)})
+            pythonProcess.stdout.on('data', function (data) {
+                console.log('' + data);
+            });
+            pythonProcess.stderr.on('data', function (data) {
+                console.log('' + data);
+            });
+            pythonProcess.on('error', function (err) {
+                console.log(err);reject(err);
+            });
 
             pythonProcess.on('exit', function (code) {
                 if (code == 0) {
                     // Operation completed successfully
-                    console.log("EPK file successfully signed with your identity")
-                    console.log("Signed EPK file is there: "+signedEPKPath)
-                    resolve(signedEPKPath)
-                }
-                else {
-                    reject('Child process exited with code ' + code)
+                    console.log("EPK file successfully signed with your identity");
+                    console.log("Signed EPK file is there: " + signedEPKPath);
+                    resolve(signedEPKPath);
+                } else {
+                    reject('Child process exited with code ' + code);
                 }
             });
-        })
+        });
     }
-}
+};

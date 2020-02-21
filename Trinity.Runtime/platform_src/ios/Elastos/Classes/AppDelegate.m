bd0b150fd3daa2435fe8a35bc622dd7362ffc052
Benjamin Piette <benjamin.piette@elastos-fusion.org>
2020-02-21 21:21:30 +0800

    Fixed openurl entry point for ios11 to handle elastos://

--- Trinity.Runtime/platform_src/ios/Elastos/Classes/AppDelegate.m
+++ Trinity.Runtime/platform_src/ios/Elastos/Classes/AppDelegate.m
@@ -37,7 +37,7 @@
     return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
-- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotationm {
+- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
     
     return [(MainViewController*)self.viewController openURL:url];
 }

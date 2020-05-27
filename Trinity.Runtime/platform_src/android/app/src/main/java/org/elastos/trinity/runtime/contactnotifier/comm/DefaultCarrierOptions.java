a6f30e608a9deef22f8089978c20bb5991df6824
raozhiming <raozhiming@elastos.org>
2020-05-27 21:34:23 +0800

    set ExpressNode for carrieroption in contactnotifier

--- Trinity.Runtime/platform_src/android/app/src/main/java/org/elastos/trinity/runtime/contactnotifier/comm/DefaultCarrierOptions.java
+++ Trinity.Runtime/platform_src/android/app/src/main/java/org/elastos/trinity/runtime/contactnotifier/comm/DefaultCarrierOptions.java
@@ -15,10 +15,9 @@ public class DefaultCarrierOptions extends Carrier.Options {
         setUdpEnabled(true);
         setPersistentLocation(path); // path is used to cache carrier data for better performance
 
+        // TODO load config from bootstraps.json?
         ArrayList<BootstrapNode> arrayList = new ArrayList<>();
-        BootstrapNode node;
-
-        node = new BootstrapNode();
+        BootstrapNode node = new BootstrapNode();
         node.setIpv4("13.58.208.50");
         node.setPort("33445");
         node.setPublicKey("89vny8MrKdDKs7Uta9RdVmspPjnRMdwMmaiEW27pZ7gh");
@@ -28,18 +27,6 @@ public class DefaultCarrierOptions extends Carrier.Options {
         node.setIpv4("18.216.102.47");
         node.setPort("33445");
         node.setPublicKey("G5z8MqiNDFTadFUPfMdYsYtkUDbX5mNCMVHMZtsCnFeb");
-        arrayList.add(node);
-
-        node = new BootstrapNode();
-        node.setIpv4("52.83.127.216");
-        node.setPort("33445");
-        node.setPublicKey("4sL3ZEriqW7pdoqHSoYXfkc1NMNpiMz7irHMMrMjp9CM");
-        arrayList.add(node);
-
-        node = new BootstrapNode();
-        node.setIpv4("52.83.127.85");
-        node.setPort("33445");
-        node.setPublicKey("CDkze7mJpSuFAUq6byoLmteyGYMeJ6taXxWoVvDMexWC");
         arrayList.add(node);
 
         node = new BootstrapNode();
@@ -54,6 +41,34 @@ public class DefaultCarrierOptions extends Carrier.Options {
         node.setPublicKey("5tuHgK1Q4CYf4K5PutsEPK5E3Z7cbtEBdx7LwmdzqXHL");
         arrayList.add(node);
 
+        node = new BootstrapNode();
+        node.setIpv4("52.83.191.228");
+        node.setPort("33445");
+        node.setPublicKey("3khtxZo89SBScAMaHhTvD68pPHiKxgZT6hTCSZZVgNEm");
+        arrayList.add(node);
+
         setBootstrapNodes(arrayList);
+
+        //Hive
+        ArrayList<ExpressNode> expressNodes = new ArrayList<>();
+        ExpressNode expNode = new ExpressNode();
+        expNode.setIpv4("ece00.trinity-tech.io");
+        expNode.setPort("443");
+        expNode.setPublicKey("FyTt6cgnoN1eAMfmTRJCaX2UoN6ojAgCimQEbv1bruy9");
+        expressNodes.add(expNode);
+
+        expNode = new ExpressNode();
+        expNode.setIpv4("ece01.trinity-tech.io");
+        expNode.setPort("443");
+        expNode.setPublicKey("FyTt6cgnoN1eAMfmTRJCaX2UoN6ojAgCimQEbv1bruy9");
+        expressNodes.add(expNode);
+
+        expNode = new ExpressNode();
+        expNode.setIpv4("ece01.trinity-tech.cn");
+        expNode.setPort("443");
+        expNode.setPublicKey("FyTt6cgnoN1eAMfmTRJCaX2UoN6ojAgCimQEbv1bruy9");
+        expressNodes.add(expNode);
+
+        setExpressNodes(expressNodes);
     }
 }
--- Trinity.Runtime/platform_src/ios/elastOS/Classes/ContactNotifier/comm/DefaultCarrierOptions.swift
+++ Trinity.Runtime/platform_src/ios/elastOS/Classes/ContactNotifier/comm/DefaultCarrierOptions.swift
@@ -3,7 +3,7 @@ import ElastosCarrierSDK
 public class DefaultCarrierOptions {
     public static func createOptions (didSessionDID: String) -> CarrierOptions {
         let options = CarrierOptions()
-        
+
         options.bootstrapNodes = [BootstrapNode]()
         options.hivebootstrapNodes = [HiveBootstrapNode]()
         options.udpEnabled = true
@@ -28,18 +28,6 @@ public class DefaultCarrierOptions {
         nodes.append(node)
 
         node = BootstrapNode()
-        node.ipv4 = "52.83.127.216"
-        node.port = "33445"
-        node.publicKey = "4sL3ZEriqW7pdoqHSoYXfkc1NMNpiMz7irHMMrMjp9CM"
-        nodes.append(node)
-
-        node = BootstrapNode()
-        node.ipv4 = "52.83.127.85"
-        node.port = "33445"
-        node.publicKey = "CDkze7mJpSuFAUq6byoLmteyGYMeJ6taXxWoVvDMexWC"
-        nodes.append(node)
-
-        node = BootstrapNode()
         node.ipv4 = "18.216.6.197"
         node.port = "33445"
         node.publicKey = "H8sqhRrQuJZ6iLtP2wanxt4LzdNrN2NNFnpPdq1uJ9n2"
@@ -51,8 +39,38 @@ public class DefaultCarrierOptions {
         node.publicKey = "5tuHgK1Q4CYf4K5PutsEPK5E3Z7cbtEBdx7LwmdzqXHL"
         nodes.append(node)
 
+        node = BootstrapNode()
+        node.ipv4 = "52.83.191.228"
+        node.port = "33445"
+        node.publicKey = "3khtxZo89SBScAMaHhTvD68pPHiKxgZT6hTCSZZVgNEm"
+        nodes.append(node)
+
         options.bootstrapNodes = nodes
-        
+/*
+        //Hive
+        var expressNodes = Array<HiveBootstrapNode>()
+        var expNode: HiveBootstrapNode
+
+        expNode = HiveBootstrapNode()
+        expNode.ipv4 = "ece00.trinity-tech.io"
+        expNode.port = "443"
+        expNode.publicKey = "FyTt6cgnoN1eAMfmTRJCaX2UoN6ojAgCimQEbv1bruy9"
+        expressNodes.append(expNode)
+
+        expNode = HiveBootstrapNode()
+        expNode.ipv4 = "ece01.trinity-tech.io"
+        expNode.port = "443"
+        expNode.publicKey = "FyTt6cgnoN1eAMfmTRJCaX2UoN6ojAgCimQEbv1bruy9"
+        expressNodes.append(expNode)
+
+        expNode = HiveBootstrapNode()
+        expNode.ipv4 = "ece01.trinity-tech.cn"
+        expNode.port = "443"
+        expNode.publicKey = "FyTt6cgnoN1eAMfmTRJCaX2UoN6ojAgCimQEbv1bruy9"
+        expressNodes.append(expNode)
+
+        options.hivebootstrapNodes = expressNodes
+*/
         return options
     }
 }
